1d1d402fd40882a3c47bd1094872465d8771f4fc
R.SIVAPACKIA <sivapackia2001@gmail.com>
2024-03-07 20:21:39 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+let order=document.querySelector(".order")
+
+function Menucard() {
+			
+	order.classList.remove("d-none")			
+	order.classList.remove("disallow")
+}
+
+function removecard() {
+order.classList.add("disallow")
+order.classList.add("d-none")
+
+}
